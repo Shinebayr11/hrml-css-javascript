e785f17b1e767cf603e7e1205707ee0093856771
Shinebayr11 <shineebayraa79@gmail.com>
2026-04-15 13:08:49 +0800

    4/15

---
+++ shine.js
@@ -0,0 +1,43 @@
+// let currentCountred = 50;
+// let currentCountblue = 50;
+
+// let red = document.querySelector("#box1");
+// let blue = document.querySelector("#box2");
+
+// function ulaan() {
+//   currentCountblue--;
+//   currentCountred++;
+//   red.textContent = `${currentCountred}%`;
+//   red.style.width = `${currentCountred}%`;
+//   blue.textContent = `${currentCountblue}%`;
+//   blue.style.width = `${currentCountblue}%`;
+// }
+
+// function tsenher() {
+//   currentCountblue++;
+//   currentCountred--;
+//   red.textContent = `${currentCountred}%`;
+//   red.style.width = `${currentCountred}%`;
+//   blue.textContent = `${currentCountblue}%`;
+//   blue.style.width = `${currentCountblue}%`;
+// }
+// document.addEventListener("keydown", function (event) {
+//   if (event.key === "ArrowRight") {
+//     ulaan();
+//   }
+
+//   if (event.key === "ArrowLeft") {
+//     tsenher();
+//   }
+// });
+
+let asaah = false;
+function towchluur() {
+  let gerelasah = document.querySelector("#gerel");
+  if (asaah) {
+    gerelasah.innerHTML = <img src="./light-on.gif" alt=""></img>;
+    asaah = false;
+  } else {
+    gerelasah.innerHTML = <img src="./light-on.gif" alt=""></img>;
+  }
+}
