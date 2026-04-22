edf4bfbeb9f8b016a866a5161c637bf97749cd8d
Shinebayr11 <shineebayraa79@gmail.com>
2026-04-22 13:42:58 +0800

    4/22

--- object.js
+++ object.js
@@ -31,41 +31,69 @@
 //    `;
 //   box.innerHTML += todoHtml;
 // }
-const person = [
-  {
-    ner: "Bat",
-    age: "21",
-    huis: "er",
-    phone: "1234",
-  },
-  {
-    ner: "Bold",
-    age: "20",
-    huis: "er",
-    phone: "4564",
-  },
-  {
-    ner: "Anu",
-    age: "22",
-    huis: "em",
-    phone: "2341",
-  },
-  {
-    ner: "Zul",
-    age: "21",
-    huis: "em",
-    phone: "6847",
-  },
-  {
-    ner: "Suh",
-    age: "19",
-    huis: "er",
-    phone: "0986",
-  },
-];
+// const person = [
+//   {
+//     ner: "Bat",
+//     age: "21",
+//     huis: "er",
+//     phone: "1234",
+//   },
+//   {
+//     ner: "Bold",
+//     age: "20",
+//     huis: "er",
+//     phone: "4564",
+//   },
+//   {
+//     ner: "Anu",
+//     age: "22",
+//     huis: "em",
+//     phone: "2341",
+//   },
+//   {
+//     ner: "Zul",
+//     age: "21",
+//     huis: "em",
+//     phone: "6847",
+//   },
+//   {
+//     ner: "Suh",
+//     age: "19",
+//     huis: "er",
+//     phone: "0986",
+//   },
+// ];
 
-for (let i = 0; i < person.length; i++) {
-  if (person[i].huis === "er") {
-    console.log(person[i]);
-  }
-}
+// for (let i = 0; i < person.length; i++) {
+//   if (person[i].huis === "er") {
+//     console.log(person[i]);
+//   }
+// }
+// let hun = {
+//   firstName: "John",
+//   lastName: "Tom",
+//   age: 22,
+//   city: "USA",
+// };
+
+// hun.city = "UB";
+// hun.email = "text@gmail.com";
+// console.log(hun);
+
+let student = {
+  name: "Shinee",
+  age: 23,
+  grades: {
+    math: 95,
+    science: 85,
+    history: 86,
+  },
+  address: "3 gol office",
+  city: "Ulaanbaatar",
+  zip: 12334,
+};
+
+delete student.address.zip;
+console.log(student);
+// console.log(Object.keys(student));
+// console.log(Object.values(student));
--- to-do.js
+++ to-do.js
@@ -1,45 +1,74 @@
-// let ongo = document.querySelector("#All");
-// let ongo1 = document.querySelector("#Active");
-// let ongo2 = document.querySelector("#Complete");
+let ongo = document.querySelector("#All");
+let ongo1 = document.querySelector("#Active");
+let ongo2 = document.querySelector("#Complete");
 
-// function allBtn() {
-//   ongo1.style.backgroundColor = "white";
-//   ongo2.style.backgroundColor = "white";
-//   ongo.style.backgroundColor = "#3c82f6";
-// }
-// function active() {
-//   ongo.style.backgroundColor = "white";
-//   ongo2.style.backgroundColor = "white";
-//   ongo1.style.backgroundColor = "#3c82f6";
-// }
-// function complete() {
-//   ongo.style.backgroundColor = "white";
-//   ongo1.style.backgroundColor = "white";
-//   ongo2.style.backgroundColor = "#3c82f6";
-// }
-
-// let bichwer = document.getElementById("tasks");
-// let input = document.getElementById("input");
-
-// function addtext() {
-//   let text = input.value;
-//   bichwer.appendChild(document.createElement("div"));
-//   bichwer.textContent = text;
-// }
+function allBtn() {
+  ongo1.style.backgroundColor = "white";
+  ongo2.style.backgroundColor = "white";
+  ongo.style.backgroundColor = "#3c82f6";
+}
+function active() {
+  ongo.style.backgroundColor = "white";
+  ongo2.style.backgroundColor = "white";
+  ongo1.style.backgroundColor = "#3c82f6";
+}
+function complete() {
+  ongo.style.backgroundColor = "white";
+  ongo1.style.backgroundColor = "white";
+  ongo2.style.backgroundColor = "#3c82f6";
+}
 
 let input = document.getElementById("input");
 let tasks = document.getElementById("tasks");
-
 function addtext() {
+  let text = input.value;
+  if (text === "") return;
+
+  let yet = document.getElementById("yet");
+  if (yet) {
+    yet.remove();
+  }
+
+  let li = document.createElement("div");
+  li.className = "a";
   let checkbox = document.createElement("input");
-  let li = document.createElement("li");
+  let delButton = document.createElement("button");
+  delButton.className = "delButton";
+  let task = document.createElement("p");
+  task.className = "task";
+  task.textContent = text;
 
+  // li.textContent = input.value;
   checkbox.type = "checkbox";
-  li.textContent = input.value;
-
-  tasks.appendChild(li);
+  li.appendChild(delButton);
+  li.appendChild(task);
   li.appendChild(checkbox);
+  tasks.appendChild(li);
+
+  input.value = "";
 }
+
+// function addtext() {
+//   let text = input.value;
+//   if (text === "") return;
+
+// let emptyText = tasks.querySelector("p");
+// if (emptyText) {
+//   emptyText.remove();
+// }
+
+//   let li = document.createElement("li");
+//   let checkbox = document.createElement("input");
+
+//   checkbox.type = "checkbox";
+
+//   li.appendChild(checkbox);
+//   li.append(" " + text);
+
+//   tasks.appendChild(li);
+
+//   input.value = "";
+// }
 
 // const todos = [
 //   {
